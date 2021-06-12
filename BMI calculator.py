3e22d31b84ad97d21f96972dda763d87546885a5
Diamonds <emydiamond007@gmail.com>
2021-06-12 13:52:45 +0300

    BMI calc upgrade from day 3

--- BMI calculator.py
+++ BMI calculator.py
@@ -6,8 +6,16 @@ num_height = float(height)
 num_weight = float(weight)
 
 BMI = round(num_weight / (num_height**2))
-# BMI = str(BMI_as_int)
-# BMI = weight / (height**2)
 
 # using F-string automatically figures the datatype to be used
-print(f"Your body mass index is {BMI}")
+
+if BMI < 18.5:
+  print(f"Your BMI is {BMI} and it means you're underweight")
+elif BMI < 25:
+  print(f"Your BMI is {BMI} and it means you have a normal weight")
+elif BMI < 30:
+  print(f"Your BMI is {BMI} and it means you're slightly overweight")
+elif BMI <= 35:
+  print(f"Your BMI is {BMI} and it means you're obese")
+else:
+  print(f"Your BMI is {BMI} which is above 35. It means you're clinically obese")
